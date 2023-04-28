c05777215b10167373d586a89c21b6018d8f5542
Nickolas Oliveira <nickolasarthur@gmail.com>
2023-04-28 23:44:24 +0100

    desfiz as alterações, não vou usar o pages

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite'
-import jsx from '@vitejs/plugin-vue-jsx'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  plugins: [jsx()],
-  base: '/SlideMaker/',
-  resolve: {
-    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx']
-  }
 })
