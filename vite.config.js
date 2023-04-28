53d1e59a9a3b7476d73eb045705498309ebbffb7
Nickolas Oliveira <nickolasarthur@gmail.com>
2023-04-28 23:36:49 +0100

    config em vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/SlideMaker/'
+  base: '/SlideMaker/',
+  resolve: {
+    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx']
+  }
 })
