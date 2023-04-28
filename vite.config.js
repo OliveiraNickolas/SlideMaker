9d2a653c161dc0b7adbb449b9d93438fdbd141f7
Nickolas Oliveira <nickolasarthur@gmail.com>
2023-04-28 23:42:28 +0100

    criado deletado e reconfigurado vite

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
+import jsx from '@vitejs/plugin-vue-jsx'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  plugins: [jsx()],
   base: '/SlideMaker/',
   resolve: {
     extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx']
